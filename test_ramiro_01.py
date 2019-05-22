e545e9916f50a83acdf5f6d699639a1ca4b3800f
Ramiro <ramirosuriano@gmail.com>
2019-05-21 23:56:01 -0300

    Agregue los archivos que nos paso feliz

--- test_ramiro_01.py
+++ test_ramiro_01.py
@@ -18,22 +18,13 @@ plt.imshow(im_in)
 
 new_im = np.zeros(im_in.shape)
 new_im = tools.convert_to('YIQ',im_in)
-"""
-new_im[:,:,0] = 0.299000*im_in[:,:,0] + 0.587000*im_in[:,:,1] + 0.114000*im_in[:,:,2]
-new_im[:,:,1] = 0.595716*im_in[:,:,0] - 0.274453*im_in[:,:,1] - 0.321263*im_in[:,:,2]
-new_im[:,:,2] = 0.211456*im_in[:,:,0] - 0.522591*im_in[:,:,1] + 0.311135*im_in[:,:,2]
 
-#plt.imshow(new_im)
-"""
-new_im[:,:,0] *= 1.5 
+print(new_im.shape)
+#new_im[:,:,0] *= 1.5 
 
 luminancia = np.zeros(im_in.shape)
 luminancia = tools.convert_to('RGB',new_im)
-"""
-luminancia[:,:,0] = 1*new_im[:,:,0] + 0.9663*new_im[:,:,1] + 0.6210*new_im[:,:,2]
-luminancia[:,:,1] = 1*new_im[:,:,0] - 0.2721*new_im[:,:,1] - 0.6474*new_im[:,:,2]
-luminancia[:,:,2] = 1*new_im[:,:,0] - 1.1070*new_im[:,:,1] + 1.7046*new_im[:,:,2]
-"""
+
 luminancia = np.clip(luminancia,0.,1.)
-plt.imshow(luminancia)
+#plt.imshow(luminancia)
 
--- tools.py
+++ tools.py
@@ -12,13 +12,15 @@ import matplotlib.pyplot as plt
 def convert_to(space,image):
     
     toYIQ = toRGB = np.zeros((3,3))
-    toYIQ = [[0.299,0.587,0.114] , [0.595716,-0.274453,-0.321263] , [0.211456,-0.522591,0.311135]] 
-    toRGB = [[1,0.9663,0.621] , [1,-0.2721,-0.6474] , [1,-1.107,1.7046]]
+    toYIQ = [[0.299, 0.587, 0.114] , [0.595716, -0.274453, -0.321263] , [0.211456, -0.522591, 0.311135]] 
+    toRGB = [[1, 0.9663, 0.621] , [1, -0.2721, -0.6474] , [1, -1.107, 1.7046]]
     
-    if (space == 'RGB'):
-        return toRGB*image
     if (space == 'YIQ'):
-        return toYIQ*image
+        return np.dot(toRGB, image)
+            
+            
+    if (space == 'RGB'):
+        return 0
     
     
     

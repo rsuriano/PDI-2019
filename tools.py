c64568021ac911525fe936ffe457d248058116fc
Tutee95 <40502293+Tutee95@users.noreply.github.com>
2019-05-27 01:55:45 -0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -12,13 +12,25 @@ import matplotlib.pyplot as plt
 def convert_to(space,image): 
     #esta funcion convierte una matriz 'image' al espacio cromático indicado en 'space'
     toYIQ = toRGB = np.zeros((3,3))
+    imageRet = np.zeros(image.shape)
     toYIQ = [[0.299,0.587,0.114] , [0.595716,-0.274453,-0.321263] , [0.211456,-0.522591,0.311135]] 
     toRGB = [[1,0.9663,0.621] , [1,-0.2721,-0.6474] , [1,-1.107,1.7046]]
     
+    
     if (space == 'RGB'):
-        return toRGB*image
+        imageRet[:,:,0] = [image[:,:,0]*[toRGB[0,:]]
+        imageRet[:,:,1] = [image[:,:,1]*[toRGB[1,:]]
+        imageRet[:,:,2] = [image[:,:,2]*[toRGB[2,:]]
+    
+        return imageRet
     if (space == 'YIQ'):
-        return toYIQ*image
+        imageRet[:,:,0] = [image[:,:,0]*[toYIQ[0,:]]
+        imageRet[:,:,1] = [image[:,:,1]*[toYIQ[1,:]]
+        imageRet[:,:,2] = [image[:,:,2]*[toYIQ[2,:]]
+        
+        return imageRet
+    
+    
     
     
     

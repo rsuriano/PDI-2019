b78792dd4c95f4709df0af26f9a0196ec8aae4cd
Ramiro <ramirosuriano@gmail.com>
2019-06-26 15:43:48 -0300

    It finally works
    El orden del codigo altera los plots, no tiene sentido pero es asi. Dejar los plots abajo de cada parte

--- slide4-luminancia.py
+++ slide4-luminancia.py
@@ -10,9 +10,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tools
 
-columnQty = 100
-escalar = 1.5
-filtro = 1
+columnQty = 10
+escalar = 0.7
+filtro = 2
 """
 1 - Raiz
 2 - Cuadrado
@@ -24,12 +24,19 @@ imgName = input("Select image name: ")
 img = imageio.imread(imgpath.format(imgName))
 img = img[:,:,:3]/255.
 
-#Extracts Y layer
-img_YIQ = tools.convert_to('YIQ', img)
+
+"""Extracts Y layer"""
+img_YIQ = tools.convert_to("YIQ", img)
 img_Y = img_YIQ[:,:,0]
 
+#Plots original image and luminance histogram
+plt.figure(1)
+plt.imshow(img)
+plt.figure(2)
+tools.histogramaY(img_Y, columnQty)
 
-#Applies luminance change to a copy of img
+
+"""Applies luminance change to a copy of img"""
 img2_YIQ = img_YIQ
 img2_YIQ[:,:,0] *= escalar
 img2_Y = np.clip(img2_YIQ[:,:,0],0.,1.)
@@ -37,8 +44,14 @@ img2_Y = np.clip(img2_YIQ[:,:,0],0.,1.)
 img2_YIQ[:,:,0] = img2_Y
 img2_RGB = tools.convert_to("RGB", img2_YIQ)
 
+#Plots altered image
+plt.figure(3)
+plt.imshow(img2_RGB)
+plt.figure(4)
+tools.histogramaY(img2_YIQ[:,:,0], columnQty)
 
-#Filters the image
+
+"""Filters the image"""
 filteredImg_YIQ = img_YIQ
 
 if filtro == 1:
@@ -48,23 +61,8 @@ if filtro == 2:
 
 filteredImg_RGB = tools.convert_to("RGB", filteredImg_YIQ)
 
-
-### PLOTS ###
-
-"""Plots original image and luminance histogram"""
-plt.figure(1)
-plt.imshow(img)
-plt.figure(2)
-tools.histogramaY(img_Y, columnQty)
-
-"""Plots altered image"""
-plt.figure(3)
-plt.imshow(img2_RGB)
-plt.figure(4)
-tools.histogramaY(img2_YIQ[:,:,0], columnQty)
-
-"""Plots filtered image"""
+#Plots filtered image
 plt.figure(5)
 plt.imshow(filteredImg_RGB)
 plt.figure(6)
-#tools.histogramaY(filteredImg_YIQ[:,:,0], columnQty)
+tools.histogramaY(filteredImg_YIQ[:,:,0], columnQty)

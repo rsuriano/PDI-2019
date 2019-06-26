2c49ddbd29d249880217181ce6ef68063d218b10
Ramiro <ramirosuriano@gmail.com>
2019-06-26 16:20:39 -0300

    It finally works + filtro lineal a trozos
    El orden del codigo altera los plots, no tiene sentido pero es asi. Dejar los plots abajo de cada parte

--- slide4-luminancia.py
+++ slide4-luminancia.py
@@ -11,8 +11,10 @@ import matplotlib.pyplot as plt
 import tools
 
 columnQty = 10
-escalar = 0.7
-filtro = 2
+escalar = 1
+filtro = 3
+Ymin = 0.1
+Ymax = 1
 """
 1 - Raiz
 2 - Cuadrado
@@ -36,7 +38,7 @@ plt.figure(2)
 tools.histogramaY(img_Y, columnQty)
 
 
-"""Applies luminance change to a copy of img"""
+"""Applies luminance change to a copy of img
 img2_YIQ = img_YIQ
 img2_YIQ[:,:,0] *= escalar
 img2_Y = np.clip(img2_YIQ[:,:,0],0.,1.)
@@ -48,7 +50,7 @@ img2_RGB = tools.convert_to("RGB", img2_YIQ)
 plt.figure(3)
 plt.imshow(img2_RGB)
 plt.figure(4)
-tools.histogramaY(img2_YIQ[:,:,0], columnQty)
+tools.histogramaY(img2_YIQ[:,:,0], columnQty)"""
 
 
 """Filters the image"""
@@ -58,6 +60,15 @@ if filtro == 1:
     filteredImg_YIQ[:,:,0] = np.sqrt(filteredImg_YIQ[:,:,0])
 if filtro == 2:
     filteredImg_YIQ[:,:,0] = np.square(filteredImg_YIQ[:,:,0])
+if filtro == 3:
+    for i in np.ndenumerate(filteredImg_YIQ[:,:,0]):
+        if i[1] < Ymin:
+            filteredImg_YIQ[i[0][0], i[0][1], 0] = 0
+        if i[1] > Ymax:
+            filteredImg_YIQ[i[0][0], i[0][1], 0] = 1
+        else:
+            filteredImg_YIQ[i[0][0], i[0][1], 0] *= 1/(Ymax-Ymin)
+    filteredImg_YIQ[:,:,0] = np.clip(filteredImg_YIQ[:,:,0],0.,1.)
 
 filteredImg_RGB = tools.convert_to("RGB", filteredImg_YIQ)
 

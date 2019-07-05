e02bbf8422f92c844ce3b5e39c7d4f0d82edbb68
Ramiro <ramirosuriano@gmail.com>
2019-07-05 14:07:56 -0300

    Update slide2.py

--- slide2.py
+++ slide2.py
@@ -10,9 +10,9 @@ import matplotlib.pyplot as plt
 import tools 
 
 
+#Carga de la imagen
 im_in = imageio.imread("images\slides2 - Luminance\Lena128C.png")
-im_in = im_in[:,:,:3]/255   #dejo las dos primeras bandas, y de la tercer banda borro una (alpha)
-                            #lo divido por 255 para normalizarlo
+im_in = im_in[:,:,:3]/255
 
 new_im = np.zeros(im_in.shape)
 new_im = tools.convert_to('YIQ',im_in)
@@ -24,6 +24,7 @@ new_im[:,:,0] *= factor_luminancia
 new_im[:,:,1] *= factor_saturacion
 new_im[:,:,2] *= 1 - factor_saturacion
 
+#Clipping
 new_im[:,:,0] = np.clip(new_im[:,:,0],0.,1.)
 new_im[:,:,1] = np.clip(new_im[:,:,1],-0.59,0.59)
 new_im[:,:,2] = np.clip(new_im[:,:,2],-0.52,0.52)

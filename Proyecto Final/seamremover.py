353995e1e1d4d2ddffaf6e636148d40760b92414
Tutee95 <40502293+Tutee95@users.noreply.github.com>
2020-09-28 03:01:48 -0300

    seamRemover v1.0
    Función para eliminar un camino
    (no está testeada)

---
+++ Proyecto Final/seamremover.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 28 01:10:48 2020
+
+@author: Usuario
+"""
+
+import numpy as np
+
+
+def seamreamover(N,roads,imagenOriginal):
+    #método para remover un camino de mínima energía almacenado en roads
+    alto, ancho, _ = imagenOriginal.shape
+    imagenAuxiliar = np.zeros(alto,ancho-1)
+    ult_fila = alto-1
+    pixel = N
+    
+    while (ult_fila>-1):
+        fila = imagenOriginal[ult_fila,:]   #bottom-up salgo de la ultima fila y voy subiendo
+        pixel = N + fila[N]                 #pixel es el que voy a eliminar
+        del fila[N]                         #elimino 'pixel'
+        imagenAuxiliar[ult_fila,:] = fila   #la fila sin 'pixel' la guardo en la nueva imagen
+        N = pixel                           #actualizo N para cuando vuelvo a pasar el loop
+        ult_fila = (ult_fila - 1)           #actualizo la ultima fila para cunaod vuelvo a pasar el loop
+    
+    return imagenAuxiliar #esta imagen entra de nuevo en seamcarver()

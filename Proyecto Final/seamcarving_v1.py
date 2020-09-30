9eff496512c58a32356983f6a84275bc91550cc8
Tutee95 <40502293+Tutee95@users.noreply.github.com>
2020-09-30 04:38:41 -0300

    remover horizonal filas

--- Proyecto Final/seamcarving_v1.py
+++ Proyecto Final/seamcarving_v1.py
@@ -41,16 +41,16 @@ imgSobelY = ndimage.convolve(imagY,sobelY)
     
 #Calculo de magnitud
 magnitudSobel = np.sqrt(np.square(imgSobelX) + np.square(imgSobelY))
-tools.plotImg(tools.clipImg(magnitudSobel), 'Energia de la imagen')
+tools.pltImg(tools.clipImg(magnitudSobel), 'Energia de la imagen')
 
 
 
 ###### EMPIEZA EL SEAM CARVING ######
-colsToDelete = 1
+colsToDelete = 15
 deletedCols = 0
 procImg = imagen
 while deletedCols<colsToDelete:
-    
+    print(deletedCols)
     #Conversion a YIQ para extraer luminancia
     imagYIQ = tools.convert_to("YIQ",procImg)
     imagY = imagYIQ[:,:,0]
@@ -74,4 +74,46 @@ while deletedCols<colsToDelete:
 
 tools.pltImg(removed, 'Imagen procesada')
 
+rowsToDelete = 50
+deletedRows = 0
 
+altura,largo = removed.shape[0:2]
+
+procImg2 = np.zeros([largo,altura,3])
+
+procImg2[:,:,0] = np.transpose(removed[:,:,0])
+procImg2[:,:,1] = np.transpose(removed[:,:,1])
+procImg2[:,:,2] = np.transpose(removed[:,:,2])
+
+while deletedRows<rowsToDelete:
+    print(deletedRows)    
+    #Conversion a YIQ para extraer luminancia
+    imagYIQ = tools.convert_to("YIQ",procImg2)
+    imagY = imagYIQ[:,:,0]
+    
+    #Filtro sobel X e Y
+    imgSobelX = ndimage.convolve(imagY,sobelX)
+    imgSobelY = ndimage.convolve(imagY,sobelY)
+    
+    #Calculo de magnitud
+    magnitudSobel = np.sqrt(np.square(imgSobelX) + np.square(imgSobelY))
+    
+    #buscador de seam con energia minima a eliminar
+    startPoint, seamMap = st.seamFinder(imagY, magnitudSobel)
+    
+    #removedor de seam
+    removed, seam = st.seamremover_revisited(int(startPoint), seamMap, procImg2)
+    done = tools.clipImg(procImg2+seam)
+    deletedRows +=1
+
+    procImg2 = removed
+
+altura,largo = removed.shape[0:2]
+
+normal = np.zeros([largo,altura,3])
+
+normal[:,:,0] = np.transpose(removed[:,:,0])
+normal[:,:,1] = np.transpose(removed[:,:,1])
+normal[:,:,2] = np.transpose(removed[:,:,2])
+
+tools.pltImg(normal, 'Imagen procesada horizontal')
